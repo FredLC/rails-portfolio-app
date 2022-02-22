84ec6b9c6b6442a304e295336a1d5cacadf69618
Fred Lefevre <fredlefevre@outlook.com>
2022-02-22 13:05:39 -0500

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Pug vexillologist lorem in wayfarers aesthetic tofu flexitarian quis edison bulb. Coloring book artisan wayfarers 8-bit sed nulla squid. Schlitz celiac brooklyn yr plaid, woke copper mug beard wayfarers ennui tumeric leggings +1. Echo park do copper mug, try-hard ut mlkshk godard man bun kinfolk lyft 90's mixtape lumbersexual schlitz. Flexitarian waistcoat iPhone culpa forage wayfarers, ea hoodie id hammock. Bicycle rights kogi tote bag vegan sed artisan, XOXO sunt. Labore farm-to-table dolor ut ugh velit keytar cronut."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Skill #{skill}",
+    percent_utilized: 20
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio item #{portfolio}",
+    subtitle: "My great project",
+    body: "Ut culpa live-edge wolf, beard plaid jean shorts wayfarers. Selfies meggings pour-over taxidermy truffaut banjo affogato nostrud. Plaid jianbing deserunt, cliche kogi snackwave health goth flannel williamsburg nostrud eu tumblr tumeric ramps. Leggings raclette taxidermy aliquip.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 portfolio items created"
+
